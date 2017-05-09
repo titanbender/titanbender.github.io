a9441a8d39e42b2d6570224802d924f83aaf22f6
Johan Bender <titanbender@gmail.com>
2017-05-09 15:44:12 +0200

    new files

---
+++ d3/des_stats_bar_new.js
@@ -0,0 +1,111 @@
+// Width and height of the canvas
+var w_bar = 800, h_bar = 500, trans_dur = 500;
+// Plotting margin
+var margin_bar = {top: 20, right: 20, bottom: 87, left: 60};
+var width_bar = w_bar - margin_bar.left - margin_bar.right;
+var height_bar = h_bar - margin_bar.top - margin_bar.bottom;
+
+// Set the ranges
+var xScale_bar = d3.scaleBand().rangeRound([10, width_bar]).padding(0.05);
+var yScale_bar = d3.scaleLinear().range([height_bar, 0]);
+
+// Define the axis
+var xAxis_bar = d3.axisBottom(xScale_bar);
+var yAxis_bar = d3.axisLeft(yScale_bar);
+
+
+//Create SVG element
+var svg_bar = d3.select("#area_bar_stats") 
+            .append("svg")
+            .attr("width", width_bar + margin_bar.left + margin_bar.right)
+            .attr("height", height_bar + margin_bar.top + margin_bar.bottom)
+            .append("g")
+            .attr("transform",  "translate(" + margin_bar.left + "," + margin_bar.top + ")");
+// tip
+var tip_bar = d3.tip().attr('class', 'd3-tip').offset([-10, 0])
+    .html(function(d) { return "Frequency: " + d.value; });
+svg_bar.call(tip_bar);  
+
+// load inital NYC data
+d3.json("https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/bar/bar_dict_NYC.json", function(error, data) {
+             
+    // scale the range of the data
+    xScale_bar.domain(data.map(function(d) { return d.key; }));
+    yMax_bar = d3.max(data, function(d) { return d.value; });
+    yScale_bar.domain([0, yMax_bar]);
+
+    //Create bars
+     svg_bar.selectAll("rect")
+        .data(data) // prositution as initial
+        .enter()
+        .append("rect")
+        .attr("x", function(d) { return xScale_bar(d.key); })
+        .attr("width", xScale_bar.bandwidth())
+        .attr("y", function(d) { return yScale_bar(d.value); })
+        .attr("height", function(d) { return height_bar - yScale_bar(d.value); })
+        .style("fill", "teal")
+        .style("hover", "black")
+        .on('mouseover', tip_bar.show)
+        .on('mouseout', tip_bar.hide)
+        ;
+
+    // add axis
+    // x-axis
+    svg_bar.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height_bar + ")")
+        .call(xAxis_bar)
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", "-.55em")
+        .attr("transform", "rotate(-90)" );
+    
+    // y-axis
+    svg_bar.append("g").attr("class", "y axis").call(yAxis_bar);
+
+    // // add title text   
+    // svg_bar.append("text")
+    //     .attr("class", "title")
+    //     .attr("x", (width_bar / 2))             
+    //     .attr("y", 10 - (margin_bar.top / 2))
+    //     .text("NYC");
+});
+
+
+
+// Toggle values
+var borough_bar = "NYC", type_bar = ""
+
+d3.select("#area_bar_stats_level1").on("change", function() {
+  borough_bar = this.value
+  toggle_bars(https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/bar/" + "bar_dict_" + borough_bar + type_bar + ".json")
+});
+d3.select("#area_bar_stats_level2").on("change", function() {
+  type_bar = this.value
+  toggle_bars(https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/bar/" + "bar_dict_" + borough_bar + type_bar + ".json")
+});
+
+
+// toggle between boroughs function
+function toggle_bars(file) {
+    d3.json(file, function(error, data) {
+    // scale the range of the data
+    yMax_bar = d3.max(data, function(d) { return d.value; });
+    yScale_bar.domain([0, yMax_bar]);
+
+    //Create bars
+     svg_bar.selectAll("rect")
+        .data(data) // prositution as initial
+        .transition() 
+        .delay(function(d, i) { return i * trans_dur/24; })
+        .duration(trans_dur) 
+        .attr("y", function(d) { return yScale_bar(d.value); })
+        .attr("height", function(d) { return height_bar - yScale_bar(d.value); });
+
+    // // Update title
+    // svg_bar.select("text.title").transition().duration(trans_dur).text(title_b);
+    //Update Y axis
+    svg_bar.select(".y.axis").transition().duration(trans_dur).call(yAxis_bar);
+    });
+}
--- d3/ts_acc.js
+++ d3/ts_acc.js
@@ -1,6 +1,6 @@
-var widthT=1000, heightT=500, trans_dur = 500;
+var widthT=800, heightT=500, trans_dur = 500;
 
-var margin_ts = {top: 20, right: 20, bottom: 110, left: 40},
+var margin_ts = {top: 20, right: 20, bottom: 0, left: 40},
     width_ts = widthT - margin_ts.left - margin_ts.right,
     height_ts = heightT - margin_ts.top - margin_ts.bottom;
 
@@ -26,7 +26,7 @@ var y = d3.scaleLinear()
 
 // legends
 // cat = [{name:"Kill", col:"red"}, {name:"Injured", col:"green"}, {name:"Total", col:"stealblue"}]
-cat = [{name:"Killed", col:"red"}, {name:"Injured", col:"green"}, {name:"Total", col:"blue"}]
+cat = [{name:"Killed", col:"red"}, {name:"Injured", col:"green"}]
 
 var legend = g.append("g")
   .attr("class", "x axis")
@@ -73,8 +73,9 @@ g.call(tip_tot);
 
 
 // insert data
+// below before "data/ts/ts_NYC.json"
 
-d3.json("data/ts/ts_ped_NYC.json", function(error, data) {
+d3.json("https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_NYC.json", function(error, data) {
   data.forEach(function(d) { d.date = parseDate(d.date); });
   if (error) throw error;
   
@@ -106,7 +107,7 @@ d3.json("data/ts/ts_ped_NYC.json", function(error, data) {
   g.append("path")
     .attr("class", "ts line tot")
     .datum(data)
-    .attr("stroke", "blue")
+    .attr("stroke", "none")
     .attr("d", line1);
 
   g.append("path")
@@ -161,103 +162,102 @@ d3.json("data/ts/ts_ped_NYC.json", function(error, data) {
     .on('mouseover', tip_tot.show)
     .on('mouseout', tip_tot.hide);
 
-  // // add title text   
-  // g.append("text")
-  //   .attr("class", "title")
-  //   .attr("x", (width_ts / 2))             
-  //   .attr("y", 10 - (margin_ts.top / 2))
-  //   .text("NYC");
+  // add title text   
+  g.append("text")
+    .attr("class", "title")
+    .attr("x", (width / 2))             
+    .attr("y", 10 - (margin.top / 2))
+    .text("NYC");
 });
 
-// Toggle values
-var borough_ts = "NYC", type_ts = "ped", type_ts_2 = "all"
-
-d3.select("#area_TimeSeries_level1").on("change", function() {
-  borough_ts = this.value
-  console.log("data/ts/ts_" + type_ts + "_" + borough_ts + ".json")
-  toggle_ts(file = "data/ts/ts_" + type_ts + "_" + borough_ts + ".json", cat = type_ts_2)
-});
-d3.select("#area_TimeSeries_level2").on("change", function() {
-  type_ts = this.value
-  console.log("data/ts/ts_" + type_ts + "_" + borough_ts + ".json")
-  toggle_ts(file = "data/ts/ts_" + type_ts + "_" + borough_ts + ".json", cat = type_ts_2)
-});
-d3.select("#area_TimeSeries_level3").on("change", function() {
-  type_ts_2 = this.value
-
-  console.log("data/ts/ts_" + type_ts + "_" + borough_ts + ".json")
-  toggle_ts(file = "data/ts/ts_" + type_ts + "_" + borough_ts + ".json", cat = type_ts_2)
-});
-
+ 
 // toggle function
-function toggle_ts(file, cat) {
-  d3.json(file, function(error, data) {
-    data.forEach(function(d) { d.date = parseDate(d.date); });
-   
-  if (error) throw error;
-  // scale the range of the data
-  var line1 = d3.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.tot); });
+function toggle_ts( key, file, title_b ) {
+  d3.select(key)
+    .on("click", function() {
+      //New values for dataset
+      d3.json(file, function(error, data) {
+        data.forEach(function(d) { d.date = parseDate(d.date); });
+       
+      if (error) throw error;
+      // scale the range of the data
+      var line1 = d3.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.tot); });
 
-  var line2 = d3.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.inj); });
+      var line2 = d3.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.inj); });
 
-  var line3 = d3.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.kill); });
+      var line3 = d3.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.kill); });
 
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain(d3.extent(data, function(d) {            
-        if (cat == "all") {return d.tot;}  // <== Add these
-        else if (cat == "tot") {return d.tot;}  // <== Add these
-        else if (cat == "inj") {return d.inj;}  // <== Add these
-        else    { return d.kill; }          // <== Add these
-    ;}));
+      x.domain(d3.extent(data, function(d) { return d.date; }));
+      y.domain(d3.extent(data, function(d) { return d.tot; }));
 
-  //Update bars
-  g.selectAll("path.ts.line.tot")
-    .datum(data).transition() .duration(trans_dur) .attr("d", line1)
-    .attr("stroke", function(d) {            
-        if (cat == "all") {return "blue"}  // <== Add these
-        else if (cat == "tot") {return "blue"}  // <== Add these
-        else    { return "none" }          // <== Add these
-    ;})
+      //Update bars
+      g.selectAll("path.ts.line.tot")
+        .datum(data).transition() .duration(trans_dur) .attr("d", line1);
 
-  g.selectAll("path.ts.line.inj")
-    .datum(data).transition() .duration(trans_dur) .attr("d", line2)
-    .attr("stroke", function(d) {            
-        if (cat == "all") {return "green"}  // <== Add these
-        else if (cat == "inj") {return "green"}  // <== Add these
-        else    { return "none" }          // <== Add these
-    ;})
+      g.selectAll("path.ts.line.inj")
+        .datum(data).transition() .duration(trans_dur) .attr("d", line2);
 
-  g.selectAll("path.ts.line.kill")
-    .datum(data).transition() .duration(trans_dur) .attr("d", line3)
-    .attr("stroke", function(d) {            
-        if (cat == "all") {return "red"}  // <== Add these
-        else if (cat == "kill") {return "red"}  // <== Add these
-        else    { return "none" }          // <== Add these
-    ;})
+      g.selectAll("path.ts.line.kill")
+        .datum(data).transition() .duration(trans_dur) .attr("d", line3);
 
-  // update tips
-  g.selectAll("circle.tip.kill")
-    .data(data).transition().duration(trans_dur)
-    .attr("cx", function(d) { return x(d.date); })
-    .attr("cy", function(d) { return y(d.kill); });
-  g.selectAll("circle.tip.inj")
-    .data(data).transition().duration(trans_dur)
-    .attr("cx", function(d) { return x(d.date); })
-    .attr("cy", function(d) { return y(d.inj); });
-  g.selectAll("circle.tip.tot")
-    .data(data).transition().duration(trans_dur)
-    .attr("cx", function(d) { return x(d.date); })
-    .attr("cy", function(d) { return y(d.tot); });
+      // update tips
+      g.selectAll("circle.tip.kill")
+        .data(data).transition().duration(trans_dur)
+        .attr("cx", function(d) { return x(d.date); })
+        .attr("cy", function(d) { return y(d.kill); });
 
-  // // Update title
-  // g.select("text.title").transition().duration(trans_dur).text(title_b);
-  //Update Y axis
-  g.select(".y.axis").transition().duration(trans_dur).call(d3.axisLeft(y));
+      g.selectAll("circle.tip.inj")
+        .data(data).transition().duration(trans_dur)
+        .attr("cx", function(d) { return x(d.date); })
+        .attr("cy", function(d) { return y(d.inj); });
+
+      g.selectAll("circle.tip.tot")
+        .data(data).transition().duration(trans_dur)
+        .attr("cx", function(d) { return x(d.date); })
+        .attr("cy", function(d) { return y(d.tot); });
+
+      // Update title
+      g.select("text.title").transition().duration(trans_dur).text(title_b);
+      //Update Y axis
+      g.select(".y.axis").transition().duration(trans_dur).call(d3.axisLeft(y));
+    });
   });
 }
+
+// TOT
+toggle_ts( key = "tot_ts_BRONX", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_tot_BRONX.json", title_b = "BRONX: Total people involved in trafic accidents")
+toggle_ts( key = "tot_ts_BROOKLYN", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_tot_BROOKLYN.json", title_b = "BROOKLYN: Total people involved in trafic accidents")
+toggle_ts( key = "tot_ts_STATEN_ISLAND", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_tot_STATEN%20ISLAND.json", title_b = "STATEN ISLAND: Total people involved in trafic accidents")
+toggle_ts( key = "tot_ts_MANHATTAN", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_tot_MANHATTAN.json", title_b = "MANHATTAN: Total people involved in trafic accidents")
+toggle_ts( key = "tot_ts_QUEENS", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_tot_QUEENS.json", title_b = "QUEENS: Total people involved in trafic accidents ")
+
+// PED
+toggle_ts( key = "ped_ts_BRONX", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_ped_BRONX.json", title_b = "BRONX: Pedestrians involved in trafic accidents")
+toggle_ts( key = "ped_ts_BROOKLYN", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_ped_BROOKLYN.json", title_b = "BROOKLYN: Pedestrians involved in trafic accidents")
+toggle_ts( key = "ped_ts_STATEN_ISLAND", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_ped_STATEN%20ISLAND.json", title_b = "STATEN ISLAND: Pedestrians involved in trafic accidents")
+toggle_ts( key = "ped_ts_MANHATTAN", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_ped_MANHATTAN.json", title_b = "MANHATTAN: Pedestrians involved in trafic accidents")
+toggle_ts( key = "ped_ts_QUEENS", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_ped_QUEENS.json", title_b = "QUEENS: Pedestrians involved in trafic accidents")
+
+// CYC
+toggle_ts( key = "cyc_ts_BRONX", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_cyc_BRONX.json", title_b = "BRONX: Cyclists involved in trafic accidents")
+toggle_ts( key = "cyc_ts_BROOKLYN", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_cyc_BROOKLYN.json", title_b = "BROOKLYN: Cyclists involved in trafic accidents")
+toggle_ts( key = "cyc_ts_STATEN_ISLAND", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_cyc_STATEN%20ISLAND.json", title_b = "STATEN ISLAND: Cyclists involved in accidents")
+toggle_ts( key = "cyc_ts_MANHATTAN", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_cyc_MANHATTAN.json", title_b = "MANHATTAN: Cyclists involved in accidents")
+toggle_ts( key = "cyc_ts_QUEENS", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_cyc_QUEENS.json", title_b = "QUEENS:  Cyclists involved in accidents")
+
+// MOT
+toggle_ts( key = "mot_ts_BRONX", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_mot_BRONX.json", title_b = "BRONX: Motorists involved in trafic accidents")
+toggle_ts( key = "mot_ts_BROOKLYN", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_mot_BROOKLYN.json", title_b = "BROOKLYN: Motorists involved in trafic accidents")
+toggle_ts( key = "mot_ts_STATEN_ISLAND", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_mot_STATEN%20ISLAND.json", title_b = "STATEN ISLAND: Motorists involved in trafic accidents")
+toggle_ts( key = "mot_ts_MANHATTAN", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_mot_MANHATTAN.json", title_b = "MANHATTAN: Motorists involved in trafic accidents")
+toggle_ts( key = "mot_ts_QUEENS", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_mot_QUEENS.json", title_b = "QUEENS: Motorists involved in trafic accidents")
+
+// NYC
+toggle_ts( key = "ts_NYC", https = "https://raw.githubusercontent.com/titanbender/titanbender.github.io/master/data/ts/ts_NYC.json", title_b = "NYC: Total people involved in trafic accidents")
+
